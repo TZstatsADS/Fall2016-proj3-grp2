cc61d68bfdd226a24a61c804c48dbda258600f6e
Rong Li <rl2779@columbia.edu>
2016-10-28 14:53:29 -0400

    Add files via upload

--- lib/Get.Main.Object.R
+++ lib/Get.Main.Object.R
@@ -1,12 +1,7 @@
-library(EBImage)
-if(i %in% 1:9) file.name = paste0("chicken_000",i,".jpg")
-
-Img =channel(readImage(file.name),"gray")
-Get.Main(Img)
 Get.Main<-function(Img,NP = 32){
 library(radiomics)
 library(EBImage)
-#---------Get Main Objects------#
+#---------Get Main Object Areas------#
 Test = Img@.Data
 H = hist(Test)
 index = which(H$density>1)
@@ -16,14 +11,19 @@ ncol = dim(Test)[2]
 Temp = as.vector(Test)
 Temp[Temp<MainColors[1] | Temp>MainColors[2]] = 0
 Temp[Temp!=0] = 1
+#------Select a submatrix for analyze--#
 Test1 = matrix(Temp,nrow,ncol)
-col.index = which.max(apply(Test1,2,sum))
-row.index = which.max(apply(Test1,1,sum))
-col.min = max(1,col.index-NP)
-col.max = min(ncol,col.index+NP-1)
-row.min = max(1,row.index-NP)
-row.max = min(nrow,row.index+NP-1)
+col.index = which.max(apply(Test1,2,sum)) 
+row.index = which.max(apply(Test1,1,sum)) 
+col.min = max(1,(col.index-NP)) 
+col.max = min(ncol,(col.index+NP-1)) 
+row.min = max(1,(row.index-NP)) 
+row.max = min(nrow,(row.index+NP-1)) 
 Main = Test[row.min:row.max,col.min:col.max]
+
+#---Try another part of Main---# #The alternative part works! This is weird!
+#Main = Test[1:10,2:15]
+
 #----Get Texture Features ----#
 Texture = glcm(Main)
 TF = calc_features(Texture)
--- lib/Get.Texture.Features.R
+++ lib/Get.Texture.Features.R
@@ -7,13 +7,26 @@ Get.Texture.Features<-function(num,Name){
     if(i %in% 10:99) file.name = paste0(Name,"_00",i,".jpg")
     if(i %in% 100:999) file.name = paste0(Name,"_0",i,".jpg")
     if(i %in% 1000:9999) file.name = paste0(Name,"_",i,".jpg")
-    A = channel(readImage(file.name),"gray")
-    Temp = Get.Main(A)
+    Img = channel(readImage(file.name),"gray")
+#---Analyze Texture Features---#
+    Temp = Get.Main(Img,NP=32)
     Features = cbind(Features,Temp)
   }
   colnames(Features) = c(rep(Name,length(num)))
   return(Features)
 }
 
-#A = Get.Texture.Features(1:5,"chicken")
+#---Extract Features---#
+Chicken.TF1 = Get.Texture.Features(1:180,"chicken")
+Chicken.TF2 = Get.Texture.Features(182:439,"chicken")
+Chicken.TF3 = Get.Texture.Features(441:797,"chicken")
+Chicken.TF4 = Get.Texture.Features(799:983,"chicken")
+Chicken.TF5 = Get.Texture.Features(985:1000,"chicken")
 
+# For chicken Pictures---Below are the failed pictures--#
+Name = "chicken"
+i = 181
+i = 440
+i = 798
+i = 984
+#
