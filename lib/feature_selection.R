7e1e2cc36dbb7608a37b6912230569e2111e0f72
Minghao Dai <md3397@columbia.edu>
2016-10-30 23:25:00 -0400

    update

--- lib/feature_selection.R
+++ lib/feature_selection.R
@@ -6,16 +6,22 @@ index <- c(1:2000)
 testindex <- sample(index, trunc(2000/3))
 lab_train <- y[-testindex]
 lab_test <- y[testindex]
+data_train <- X[-testindex, ]
+data_test <- X[testindex, ]
 
-ft.pca <- prcomp(X)
+ft.pca <- prcomp(data_train)
+data_test_pca <- t(apply(data_test, 1, function(x) 
+                    x-as.vector(ft.pca$center))) %*% 
+                    as.matrix(ft.pca$rotation)
+
 cu.var <- summary(ft.pca)$importance[3,]
-
 cu.var.cutoff <- seq(0.6, 0.95, by = 0.05)
 l.cutoff <- length(cu.var.cutoff)
 pca.rate <- rep(NA, l.cutoff)
 
 
-traintest <- function(train, test, data_train, lab_train, data_test, lab_test){
+traintest <- function(train, test, data_train_pca, 
+                      lab_train, data_test_pca, lab_test){
   train.re <- train(data_train, lab_train)
   test.re <- test(train.re, data_test)
 
@@ -30,13 +36,12 @@ for (i in 1:l.cutoff){
   npc <- min(which(cu.var > cutoff))
   X.pca <- as.data.frame(ft.pca$x)[,1:npc]
   
-  data_train <- X.pca[-testindex, ]
-  data_test <- X.pca[testindex, ]
-  
+  data_train_pca <- X.pca
   
   ##use traintest() to train and test your model under different pca cutoff
   ##change train_svm, test_svm below
-  pca.rate[i] <- traintest(train_svm, test_svm, data_train, lab_train, data_test, lab_test)
+  pca.rate[i] <- traintest(train_svm, test_svm, data_train_pca, 
+                           lab_train, data_test_pca, lab_test)
 }
 
 cutoff_best <- cu.var.cutoff[which.min(pca.rate)]
